60f745e70f68fdfce24d968e0aa8b0cb010ecf1b
HenrikThunbo10 <henningthunbo@hotmail.com>
2024-10-08 14:22:32 +0200

    .

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -7,6 +7,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("persons");
+        // EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("persons");
+        System.out.println("Hello World!");
     }
 }
